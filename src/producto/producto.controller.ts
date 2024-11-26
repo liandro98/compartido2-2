3a7ecf85ec6f74df403bed40a29c204d3ec46638
liandro98 <miguelalejandroespinosa7@gmail.com>
2024-11-26 02:39:44 -0600

    Delete y validaciones de nombres duplicados, categorias con estatus false

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -4,14 +4,20 @@ import { Producto } from '@prisma/client';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { UpdateProductoDto } from './dto/update-producto.dto';
 
-@Controller('producto')
+@Controller('api/producto')
 export class ProductoController {
   constructor(private readonly productoService: ProductoService) {}
 
-  // Listar productos con filtro por categoría
+  // Listar productos 
   @Get('/')
   async obtenerProductos() {
     return await this.productoService.obtenerProductos();
+  }
+
+  // Listar productos por categoría
+  @Get('/OrdCatego/:cveCategoria')
+  async obtenerProductoCategoria(@Param('cveCategoria', ParseIntPipe) cveCategoria:number) {
+    return await this.productoService.obtenerProductoPorCategoria(cveCategoria);
   }
 
   // Crear un producto
@@ -20,19 +26,15 @@ export class ProductoController {
     return await this.productoService.crearProducto(data);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productoService.findOne(+id);
-  }
-
   // Actualizar producto
   @Patch(':cveProducto')
   async actualizarCategorias(@Param('cveProducto', ParseIntPipe) cveProducto:number, @Body() producto: Producto) {
     return await this.productoService.actualizar(cveProducto, producto );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productoService.remove(+id);
-  }
+  // Eliminar producto
+  @Delete(':cveProducto')
+    async eliminarCategorias(@Param('cveProducto', ParseIntPipe) cveProducto: number) {
+        return await this.productoService.eliminar(cveProducto);
+    }
 }
--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -10,24 +10,70 @@ export class ProductoService {
   constructor(private prisma: PrismaService){ }
 
   // Obtener productos con filtro por categoría (excluyendo categorías inactivas)
-  async obtenerProductos(){
-    return await this.prisma.producto.findMany();
+  async obtenerProductos() {
+    return await this.prisma.producto.findMany({
+      where: {
+        categoria: {
+          activo: true,  
+        },
+      },
+      include: {
+        categoria: true,  
+      },
+    });
   }
 
-   // Crear un nuevo producto
-   async crearProducto(producto: Producto) {
-    return this.prisma.producto.create({  data: {
+  // Listar productos por categoría
+  async obtenerProductoPorCategoria(cveCategoria: number) {
+    return await this.prisma.producto.findMany({
+      where: {
+        cveCategoria: cveCategoria,  
+        categoria: {
+          activo: true,  
+        },
+      },
+      include: {
+        categoria: true, 
+      },
+    });
+  }
+
+  // Crear un nuevo producto
+  async crearProducto(producto: Producto) {
+    // Verificar si la categoría existe y está activa
+  const categoria = await this.prisma.categoria.findUnique({
+    where: {
+      cveCategoria: producto.cveCategoria,
+    },
+  });
+
+  if (!categoria || !categoria.activo) {
+    throw new Error('La categoría no existe o no está activa');
+  }
+
+  // Verificar si ya existe un producto con el mismo nombre en la categoría
+  const productoExistente = await this.prisma.producto.findFirst({
+    where: {
+      descripcion: producto.descripcion,
+      cveCategoria: producto.cveCategoria,
+    },
+  });
+
+  if (productoExistente) {
+    throw new Error('Ya existe un producto con ese nombre en la categoría seleccionada');
+  }
+
+  // Si todo está bien, proceder a crear el nuevo producto
+  return await this.prisma.producto.create({
+    data: {
       descripcion: producto.descripcion,
       precio: producto.precio,
-      cantidad : producto.cantidad,
-      cveCategoria : producto.cveCategoria
-  } });
+      cantidad: producto.cantidad,
+      cveCategoria: producto.cveCategoria,
+    },
+  });
   }
 
-
-  findOne(id: number) {
-    return `This action returns a #${id} producto`;
-  }
 
   // Actualizar un producto existente
   async actualizar(cveProducto: number, producto: Producto){
@@ -37,9 +83,15 @@ export class ProductoService {
         },
         data: producto
     })
+  }
+
+  // Elimina un prodcuto
+  async eliminar(cveProducto: number){
+    return await this.prisma.producto.delete({
+        where: {
+          cveProducto: cveProducto
+        }
+    })
 }
 
-  remove(id: number) {
-    return `This action removes a #${id} producto`;
-  }
 }
